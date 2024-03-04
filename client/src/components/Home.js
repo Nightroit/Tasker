b75b061bc57bdd31d21837151bcc11b46be211b2
Shivam <s.sharma26626@gmail.com>
2024-03-04 14:48:32 +0530

    list and delete button implemnet

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,9 +1,16 @@
-import React, {useState} from 'react' 
-import { View, Text, StyleSheet, TextInput } from 'react-native'
+import React, {useState} from 'react'
+import { View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native'
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const Home = () => {
 
   const [text, setInputText] = useState()
+  const [list, setList] = useState(['hi'])
+
+  const onPress = () => {
+    setList([text, ...list])
+    setInputText("")
+  }
 
   return (
     <View style = {styles.container}>
@@ -15,9 +22,20 @@ const Home = () => {
             value = {text}
             style = {styles.inputField}
             >
-
           </TextInput>
-          <Text>Hi</Text>
+
+          <TouchableOpacity onPress={onPress} style={styles.button}>
+            <Icon name="plus" size={20} color="black" style = {styles.iconStyle} />
+          </TouchableOpacity>
+
+        </View>
+        <View>
+          {list.map((str, idx) => (
+            <View style = {styles.listButton}>  
+              <Text key = {idx}>{str}</Text>
+              <Icon style = {styles.deleteIcon} name="delete-outline" size={25} color="red" />
+            </View>            
+          ))}
         </View>
       </View>
     </View>
@@ -33,6 +51,7 @@ const styles = {
     height: '100%'
   },
   inputField: {
+    width: '90%',
     marginTop: 20,
     borderWidth: 2,          // Border width
     borderColor: 'black',    // Border color
@@ -40,7 +59,27 @@ const styles = {
     padding: 10,             // Padding inside the container
   }, 
   todoContainer: {
+    flexDirection: 'row', // Set flexDirection to 'row' to have items in the same line
+  }, 
+  button: {
+    flex: 1, 
+    alignItems: 'center', 
+    justifyContent: 'center', 
+    marginTop: 20,
+    borderWidth: 2,          // Border width
+    borderColor: 'black',    // Border color
+    borderRadius: 10,
+    marginLeft: 5,
+    width: '10%'
+  },
+  deleteIcon: {
     
-  }
+    color: 'black', 
+    height: '2px',
+    width: '2px'
+  },
+
+  
 };
+
 export default Home;
